df3d15679b3585f47bf0ac5346dfa4a34ee20715
VaibhavTiwari300304 <vaibtiwari30@gmail.com>
2026-02-13 16:36:15 +0530

    react

---
+++ react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <> 
+      <h1 id='child1'>Hello World</h1>
+      <h2 id='child2'>Hello</h2>
+    </>
+  )
+}
+
+export default App
